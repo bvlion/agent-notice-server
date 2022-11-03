5b5267ba517d5e290fe5d7b99dba3ebdb0a41c5e
bvlion <bvltiggeari@ambitious-i.net>
2022-11-03 12:23:31 +0900

    Initialize

---
+++ src/main/kotlin/net/ambitious/agentnotice/AgentNoticeApplication.kt
@@ -0,0 +1,11 @@
+package net.ambitious.agentnotice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgentNoticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<AgentNoticeApplication>(*args)
+}
---
+++ src/test/kotlin/net/ambitious/agentnotice/AgentNoticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.ambitious.agentnotice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgentNoticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
